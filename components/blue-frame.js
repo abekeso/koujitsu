2fd6d858889251567f5b43dcf94d625d6f5eb5af
abekeso <abekesosuke@gmail.com>
2024-01-19 19:30:24 +0900

    文字を変更しました

--- components/blue-frame.js
+++ components/blue-frame.js
@@ -1,9 +1,7 @@
-import styles from 'styles/nested-layout.module.css'
-import Container from 'components/container'
-import Link from 'next/link'
+import styles from "styles/nested-layout.module.css";
+import Container from "components/container";
+import Link from "next/link";
 
-export default function BlueFrame({ children }) { 
-    return (
-        <div classname= { styles.frame } ></div>
-
+export default function BlueFrame({ children }) {
+  return <div classname={styles.frame}></div>;
 }
